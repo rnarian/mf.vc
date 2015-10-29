da64d24ee386fe65d0832720fbda99da4f944354
Marian Friedmann <marian.friedmann@gmail.com>
2015-10-29 14:29:51 +0100

    Add form js

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,4 +21,36 @@ $(function() {
     $('.modal').removeClass('modal--open');
     $('.post-it').removeClass('post-it--hidden');
   });
+
+  $('form').submit(function(e) {
+    e.preventDefault();
+
+    var formMail = $(this).find('input'),
+        formMsg  = $(this).find('textarea');
+
+    var formData = {
+      mail: formMail.val(),
+      message: formMsg.val()
+    };
+
+    var posting = $.post( '../../mail.php', formData );
+
+    posting.done(function( data ) {
+      if (data == 'Mail sent successfully') {
+        $('.modal').removeClass('modal--open');
+        $('.post-it').removeClass('post-it--hidden');
+        setTimeout(function() {
+          $('.js-form-error').text('');
+          formMail.val('');
+          formMsg.val('');
+        },300);
+      } else {
+        $('.js-form-error').text(data);
+        $('.form').removeClass('error');
+        setTimeout(function() {
+          $('.form').addClass('error');
+        }, 10);
+      }
+    });
+  });
 });
