3c18d14557f8f9514ee3d08a129c24e65bcd9ed1
Marian Friedmann <marian.friedmann@gmail.com>
2015-10-29 14:29:23 +0100

    Add modal open/close functions

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,4 +7,18 @@
 
 $(function() {
 
+
+
+
+  $('.open-modal').click(function(e) {
+    e.preventDefault();
+    $('.modal').addClass('modal--open');
+    $('.post-it').addClass('post-it--hidden');
+  });
+
+  $('.close-modal').click(function(e) {
+    e.preventDefault();
+    $('.modal').removeClass('modal--open');
+    $('.post-it').removeClass('post-it--hidden');
+  });
 });
