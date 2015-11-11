c66c86bbd1d384cc16d990640ccd6cb3208c1909
Marian Friedmann <marian.friedmann@gmail.com>
2015-11-11 14:25:02 +0100

    Add grunt-modernizr task

--- Gruntfile.js
+++ Gruntfile.js
@@ -96,8 +96,7 @@ module.exports = function(grunt) {
         uglify: {
             all: {
                 files: {
-                    '<%= dirs.js %>/build.js': ['<%= dirs.js %>/build.js'],
-                    '<%= dirs.js %>/modernizr.js': ['<%= dirs.bower %>/modernizr/modernizr.js']
+                    '<%= dirs.js %>/build.js': ['<%= dirs.js %>/build.js']
                 }
             }
         },
@@ -132,6 +131,20 @@ module.exports = function(grunt) {
                       "wave-border": [".modal:before"]
                     }
                 }
+            }
+        },
+
+        // modernizr
+        modernizr: {
+            dist: {
+
+                // Path to save out the built file
+                dest: '<%= dirs.js %>/modernizr.js',
+                crawl : false,
+                uglify: true,
+                options : [
+                    "html5shiv"
+                ],
             }
         },
 
@@ -187,7 +200,7 @@ module.exports = function(grunt) {
         }
     });
 
-    grunt.registerTask('default', ['sass:build', 'autoprefixer', 'concat', 'uglify', 'imagemin']);
+    grunt.registerTask('default', ['sass:build', 'autoprefixer', 'concat', 'uglify', 'imagemin', 'modernizr']);
     grunt.registerTask('dev', ['connect', 'watch', 'notify']);
     grunt.registerTask('dev:sync', ['browser_sync', 'watch', 'notify']);
 };
